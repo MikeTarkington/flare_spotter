f2245fdbbf5cdbf98f38c3ea52f2c8e8eb9c165c
Mike Tarkington <tarkmike@gmail.com>
2019-01-18 00:30:19 -0500

    convert notes on features to build in the agent_log_errors.py file into current planned features for Readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # flare_spotter
+
+- Requires Installation of Python3
+
+This script will quickly identify all unique errors found in Datadog Agent log files produced by a flare. Running the script opens a prompt for file selection on macOS that is used to select the unzipped folder of the flare contents.
+
+Navigate to file directory in terminal and run:
+`python3 agent_log_errors.py`
+
+Some Planned Features (as of 1/18/19)
+- add first and last time stamp to the outputs of each unique error log
+- support arguments passed with the run command that would enable search for unquie logs with a particular term and also allow for combining the search for error logs containing a particular term (particularly useful for finding logs related to a particular check)
+- add count of occurrences for each error
+    - show most common agent, integration, error from optional argument sorted by counts
+- Identify errors found in runtime config dump
+- Summary of differences between runtime config dump against datadog.yaml
+- compare config check.log vs yaml for each integration config to determine possible config errors (beyond just yaml)
+    - config check may not have a value that looks to be correctly configed and passed yaml check
+    - would need to use python yaml parser to create dict and check it against dict from config yaml
--- agent_log_errors.py
+++ agent_log_errors.py
@@ -13,8 +13,6 @@ selected_path = filedialog.askdirectory()
 # selected_path = filedialog.askopenfilename()
 
 for dir in os.walk(selected_path):
-    print(dir[1])
-    # print(type(dir[1][1]))
     if "logs" in dir[1]:
         logs_path = f"{dir[0]}/logs"
     elif "log" in dir[1]:
@@ -38,7 +36,7 @@ for file in os.listdir(logs_directory):
     match_list = []
     with open(file_path) as file:
         for line in file:
-            if "ERROR" in line:
+            if "ERROR" in line: #function to abstract out this step perhaps
                 if len(match_list) == 0:
                     match_list.append(strip_timestamp(line))
                 elif strip_timestamp(line) not in match_list:
@@ -51,3 +49,17 @@ for file in os.listdir(logs_directory):
     for error in match_list:
         print(error)
 
+# arg parse module
+# first and last timestamp of error type
+# multi-string search to find combination of error with another term by adding an optional arg
+# show me most common agent, most common integration, most common error from optional argument
+# recommends sorting by count
+
+# compare config check.log vs yaml for each integration config to determine possible config errors (beyond just yaml)
+    # config check may not have a value that looks to be correctly configed and passed yaml check
+    # would need to use python yaml parser to create dict and check it against dict from config yaml
+# summary of differences between runtime config dump against datadog.yaml
+
+# try to use python modules
+# https: // docs.python.org/2/library/argparse.html
+# https: // pyyaml.org/wiki/PyYAMLDocumentation
